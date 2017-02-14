bac0bf8c1b7739726c21525f845d400010f351af
Daniel Naab <danielnaab@gmail.com>
2017-02-14 09:08:58 -0600

    Add Flickr and other layout tweaks

---
+++ source/javascripts/flickr.js
@@ -0,0 +1,44 @@
+window.initFlickr = function (user, album, limit) {
+    'use strict';
+
+    function Flickr() {
+        this.init()
+    }
+
+    Flickr.prototype = {
+        init: function () {
+            window.getPhotos = this.getPhotos
+            this.getJSON()
+        },
+        getJSON: function () {
+            //var src = 'http://api.flickr.com/services/feeds/photoset.gne?nsid=' + this.user + '&set=' + this.album + '&format=json&jsoncallback=getPhotos'
+            var src = 'http://api.flickr.com/services/feeds/photoset.gne?nsid=' + user + '&set=' + album + '&format=json&jsoncallback=getPhotos'
+            var script = document.createElement('script')
+                script.src = src
+                document.body.appendChild(script)
+        },
+        getPhotos: function (data) {
+            if (data && data.items) {
+                var title = data.title
+                var items = data.items
+                var albumTitle = title.replace('Content from ', '')
+                var html = '<h3>' + albumTitle + '</h3>'
+                    html += '<div class="images">'
+
+                for (var i = 0; i < items.length; i++) {
+                    var item = items[i]
+                    if (i <= limit) {
+                        html += "<a target=_blank href='" + item.link + "'><img src='" + item.media.m + "' alt=' /></a>"
+                    }
+                }
+                html += '</div>'
+
+                document.querySelector('#flickr').innerHTML = html
+            }
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        new Flickr()
+    })
+}
