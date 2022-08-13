4efd9f3b9d9696c74d841db8ee4b8d6b5bed4ebf
getitdunnbot <dan@getitdunnrun.com>
2022-08-12 20:06:03 -0500

    Use manual push to gh-pages instead of middleman-deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,12 @@
 git config --global user.email "dan@getitdunnrun.com"
 git config --global user.name "getitdunnbot"
 
-bundle exec middleman deploy
+pushd build
+rm -rf .git
+git init .
+git remote add origin git@github.com:danielnaab/getitdunn.git
+git checkout -b gh-pages
+git add .
+git commit -m "getitdunnbot automated build"
+git push --force origin gh-pages
+popd
