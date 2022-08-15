f16ce77176af0f805cf09871ceecfb7d401de0d3
getitdunnbot <dan@getitdunnrun.com>
2022-08-15 15:37:10 -0500

    Set header color, update flickr api to use https

--- source/javascripts/flickr.js
+++ source/javascripts/flickr.js
@@ -1,44 +1,58 @@
 window.initFlickr = function (user, album, limit) {
-    'use strict';
+  "use strict";
 
-    function Flickr() {
-        this.init()
-    }
+  function Flickr() {
+    this.init();
+  }
 
-    Flickr.prototype = {
-        init: function () {
-            window.getPhotos = this.getPhotos
-            this.getJSON()
-        },
-        getJSON: function () {
-            //var src = 'http://api.flickr.com/services/feeds/photoset.gne?nsid=' + this.user + '&set=' + this.album + '&format=json&jsoncallback=getPhotos'
-            var src = 'http://api.flickr.com/services/feeds/photoset.gne?nsid=' + user + '&set=' + album + '&format=json&jsoncallback=getPhotos'
-            var script = document.createElement('script')
-                script.src = src
-                document.body.appendChild(script)
-        },
-        getPhotos: function (data) {
-            if (data && data.items) {
-                var items = data.items
-                var title = data.title
-                var albumTitle = title.replace('Content from ', '')
-                var html = '<h3>Flickr: <a target=_blank href="' + data.link + '">' + albumTitle + '</a></h3>'
-                html += '<div class="images">'
+  Flickr.prototype = {
+    init: function () {
+      window.getPhotos = this.getPhotos;
+      this.getJSON();
+    },
+    getJSON: function () {
+      var src =
+        "https://api.flickr.com/services/feeds/photoset.gne?nsid=" +
+        user +
+        "&set=" +
+        album +
+        "&format=json&jsoncallback=getPhotos";
+      var script = document.createElement("script");
+      script.src = src;
+      document.body.appendChild(script);
+    },
+    getPhotos: function (data) {
+      if (data && data.items) {
+        var items = data.items;
+        var title = data.title;
+        var albumTitle = title.replace("Content from ", "");
+        var html =
+          '<h3>Flickr: <a target=_blank href="' +
+          data.link +
+          '">' +
+          albumTitle +
+          "</a></h3>";
+        html += '<div class="images">';
 
-                for (var i = 0; i < items.length; i++) {
-                    var item = items[i]
-                    if (i <= limit) {
-                        html += "<a target=_blank href='" + item.link + "'><img src='" + item.media.m + "' alt=' /></a>"
-                    }
-                }
-                html += '</div>'
-
-                document.querySelector('#flickr').innerHTML = html
-            }
+        for (var i = 0; i < items.length; i++) {
+          var item = items[i];
+          if (i <= limit) {
+            html +=
+              "<a target=_blank href='" +
+              item.link +
+              "'><img src='" +
+              item.media.m +
+              "' alt=' /></a>";
+          }
         }
-    }
+        html += "</div>";
 
-    document.addEventListener('DOMContentLoaded', function () {
-        new Flickr()
-    })
-}
+        document.querySelector("#flickr").innerHTML = html;
+      }
+    },
+  };
+
+  document.addEventListener("DOMContentLoaded", function () {
+    new Flickr();
+  });
+};
